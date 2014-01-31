e0e0148ccf8ab0afeb9d9d14e622ca24601d3703
vygis <vygintas.ranonis@gmail.com>
2014-01-31 14:25:58 +0000

    Initial config & readme changes

--- server/config.js
+++ server/config.js
@@ -6,12 +6,12 @@ module.exports = {
     apiKey: 'tpDsSDGqQP1vbFfyQLlwAuv29s3x54h_'          // Our MongoLab API key
   },
   security: {
-    dbName: 'vygis_angular_app',                        // The name of database that contains the security information
+    dbName: 'exhibition-finder',                        // The name of database that contains the security information
     usersCollection: 'users'                            // The name of the collection contains user information
   },
   server: {
-    listenPort: 3000,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
-    securePort: 8433,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
+    listenPort: 3001,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+    securePort: 8434,                                   // The HTTPS port on which the server is to listen (means that the app is at https://localhost:8433 for instance)
     distFolder: path.resolve(__dirname, '../client/dist'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
     staticUrl: '/static',                               // The base url from which we serve static files (such as js, css and images)
     cookieSecret: 'angular-app'                         // The secret for encrypting the cookie
