e7546b571e880ebb25c535f7e5b9da0f339ae534
vygis <vygintas.ranonis@gmail.com>
2014-02-12 17:22:10 +0000

    Added some mongoose models.

---
+++ server/lib/models/event.js
@@ -0,0 +1,64 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Article Schema
+ */
+var EventSchema = new Schema({
+    creationDate: {
+        type: Date,
+        default: Date.now
+    },
+    lastModificationDate: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    modifier: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    venues: {
+        type: Schema.ObjectId,
+        ref: 'Venue'
+    }
+});
+
+/**
+ * Validations
+ */
+EventSchema.path('title').validate(function(title) {
+    return title.length;
+}, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+EventSchema.statics = {
+    load: function(id, cb) {
+        this.findOne({
+            _id: id
+        }).populate('creator', 'name username')
+            .populate('modifier', 'name username')
+            .exec(cb);
+    }
+};
+
+mongoose.model('Event', EventSchema);
